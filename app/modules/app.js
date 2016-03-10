03459f8ac0a5d67bc30ae9e0aff455c5e4672203
Igor Sergiienko <Igor_Sergiienko@epam.com>
2016-03-10 19:49:26 +0200

    added gulp norm

--- app/modules/app.js
+++ app/modules/app.js
@@ -1,5 +1,5 @@
 (function(){
-	var app = angular.module('app', ['ui.router']);
+	var app = angular.module('app', []);
 
 	app.value('model', {
 		posts: [],
--- gulpfile.js
+++ gulpfile.js
@@ -3,52 +3,55 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var watch = require('gulp-watch');
-var webserver = require('gulp-webserver');
+var nodemon = require('gulp-nodemon');
+var open = require('gulp-open');
 
 
 // paths
 var paths = {
 	localhost: 'http://localhost:8000/index.html',
 	angular: 'app/vendors/angular/angular.min.js',
-	route: 'app/vendors/angular-ui-router/release/angular-ui-router.min.js',
 	app: 'app/modules/**/*.js',
-	dist: 'app/dist'
+	dist: 'app/dist',
+	port: 'http://localhost:8080/',
 };
 
 
 
 
-// default 
-
-
+// concat 
 gulp.task('concat', function() {
-	return gulp.src([ paths.angular, paths.route, paths.app ])
+	return gulp.src([ paths.angular, paths.app ])
 		.pipe(concat('app.js'))
 		.pipe(gulp.dest(paths.dist));
 });
 
 
 
-gulp.task('webserver', ['concat'], function() {
-	return gulp.src('app')
-		.pipe(webserver({
-			livereload: true,
-			directoryListing: true,
-			open: paths.localhost
-		}));
+//nodemon
+gulp.task('nodemon', function() {
+	nodemon({
+		script: 'server.js'
+	});
 });
-
-
 
 // watch files, transpile if one of them changes
 gulp.task('watch', function() {
 	gulp.watch(paths.app, ['concat']);
 });
 
+//open
+gulp.task('open', ['concat'], function(){
+	var options = {
+		app: 'chrome',
+		uri: paths.port
+	};
+	gulp.src('').pipe(open(options));
+});
 
 
-// The default task is 'watch'
-gulp.task('default', ['concat', 'watch']);
+// default task 
+gulp.task('default', ['open', 'nodemon', 'watch']);
 
 
 
